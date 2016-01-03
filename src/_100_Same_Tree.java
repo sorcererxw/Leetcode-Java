84b26d094c0c5258eb209f65f3e4e4454ba69be1
SorcererXW <pqyljn@hotmail.com>
2016-01-03 21:30:43 +0800

    AC 100 same tree

---
+++ src/_100_Same_Tree.java
@@ -0,0 +1,29 @@
+/**
+ * Created by Sorcerer on 2016/1/3 0003.
+ */
+public class _100_Same_Tree {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null || q == null) {
+            if (p == null && q == null) {
+                return true;
+            } else {
+                return false;
+            }
+        } else if (p.val == q.val) {
+            return isSameTree(p.left, q.left) & isSameTree(p.right, q.right);
+        } else {
+            return false;
+        }
+    }
+}
