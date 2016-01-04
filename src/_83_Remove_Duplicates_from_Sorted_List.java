3a471aabfe8fa220f1b2394c22a92afa203c06bd
SorcererXW <pqyljn@hotmail.com>
2016-01-04 18:03:21 +0800

    AC 83 remove duplicates from sorted list

---
+++ src/_83_Remove_Duplicates_from_Sorted_List.java
@@ -0,0 +1,28 @@
+/**
+ * Created by Sorcerer on 2016/1/4 0004.
+ * <p>
+ * https://leetcode.com/problems/remove-duplicates-from-sorted-list/
+ */
+public class _83_Remove_Duplicates_from_Sorted_List {
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode tmp = head;
+        while (head != null) {
+            if (head.next != null && head.next.val == head.val) {
+                head.next = head.next.next;
+            } else {
+                head = head.next;
+            }
+        }
+        return tmp;
+    }
+}
