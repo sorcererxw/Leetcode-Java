aec98cbae7aeb7ffc5a463e4792dbe0d80c3cd1e
SorcererXW <pqyljn@hotmail.com>
2016-01-04 13:18:07 +0800

    AC 235 lowest common ancestor of a binary search tree

---
+++ src/_235_Lowest_Common_Ancestor_of_a_Binary_Search_Tree.java
@@ -0,0 +1,37 @@
+/**
+ * Created by Sorcerer on 2016/1/4 0004.
+ * <p>
+ * https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
+ */
+public class _235_Lowest_Common_Ancestor_of_a_Binary_Search_Tree {
+
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) {
+            return null;
+        }
+        if (root.val >= p.val && root.val <= q.val) {
+            return root;
+        }
+        if (root.val <= p.val && root.val >= q.val) {
+            return root;
+        }
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (left == null) {
+            return right;
+        } else {
+            return left;
+        }
+    }
+}
