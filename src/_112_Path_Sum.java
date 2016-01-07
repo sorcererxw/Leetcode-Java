f628a3763d17d60dbf1b896d7d31211c0ad5daeb
SorcererXW <pqyljn@hotmail.com>
2016-01-07 12:44:43 +0800

    AC 119 pascal's triangle II

---
+++ src/_112_Path_Sum.java
@@ -0,0 +1,13 @@
+import models.TreeNode;
+
+/**
+ * Created by Sorcerer on 2016/1/7 0007.
+ *
+ * https://leetcode.com/problems/path-sum/
+ */
+public class _112_Path_Sum {
+
+    public boolean hasPathSum(TreeNode root, int sum) {
+        return false;
+    }
+}
--- src/_119_Pascals_Triangle_II.java
+++ src/_119_Pascals_Triangle_II.java
@@ -1,5 +1,36 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Sorcerer on 2016/1/7 0007.
+ * <p>
+ * https://leetcode.com/problems/pascals-triangle-ii/
  */
 public class _119_Pascals_Triangle_II {
+
+    public static void main(String[] args) {
+        System.out.println(getRow(4));
+    }
+
+    public static List<Integer> getRow(int rowIndex) {
+        rowIndex += 1;
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        int tmp;
+        for (int i = 1; i < rowIndex; i++) {
+            tmp = 1;
+            for (int j = 1; j <= i; j++) {
+                if (j == 0) {
+                    continue;
+                } else if (j == i) {
+                    list.add(1);
+                } else {
+                    int t = tmp;
+                    tmp = list.get(j);
+                    list.set(j, t + list.get(j));
+                }
+            }
+        }
+        return list;
+    }
 }
