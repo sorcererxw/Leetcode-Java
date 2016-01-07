7e04de6c57641ad27d5adca74dcaed5d04db0f77
SorcererXW <pqyljn@hotmail.com>
2016-01-07 13:09:28 +0800

    AC 112 path sum

--- src/_112_Path_Sum.java
+++ src/_112_Path_Sum.java
@@ -1,13 +1,38 @@
+import com.sun.istack.internal.Nullable;
 import models.TreeNode;
 
 /**
  * Created by Sorcerer on 2016/1/7 0007.
- *
+ * <p>
  * https://leetcode.com/problems/path-sum/
  */
 public class _112_Path_Sum {
 
-    public boolean hasPathSum(TreeNode root, int sum) {
-        return false;
+    public static void main(String[] args) {
+        TreeNode treeNode = new TreeNode(-2);
+        treeNode.right = new TreeNode(-3);
+        System.out.println(hasPathSum(treeNode, -2));
+    }
+
+    public static boolean hasPathSum(TreeNode root, int sum) {
+        if (root == null) {
+            return false;
+        }
+        return calculate(root, sum);
+    }
+
+    public static boolean calculate(TreeNode root, int sum) {
+        if (sum - root.val == 0 && root.left == null && root.right == null) {
+            return true;
+        }
+        boolean flag = false;
+        if (root.left != null) {
+            flag = flag || calculate(root.left, sum - root.val);
+        }
+
+        if (root.right != null && flag == false) {
+            flag = flag || calculate(root.right, sum - root.val);
+        }
+        return flag;
     }
 }
