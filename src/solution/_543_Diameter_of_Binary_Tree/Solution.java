5c67fe511157d65b67f4a5ae130cc6b75567eb26
SorcererXW <tujipqy@gmail.com>
2019-04-03 09:08:28 +0800

    ac 543

--- src/solution/_543_Diameter_of_Binary_Tree/Solution.java
+++ src/solution/_543_Diameter_of_Binary_Tree/Solution.java
@@ -1,4 +1,23 @@
 package solution._543_Diameter_of_Binary_Tree;
 
-public class Solution {
+import models.TreeNode;
+
+class Solution {
+    private int result = 0;
+
+    private int deepest(TreeNode root) {
+        if (root == null) return 0;
+        int left = deepest(root.left);
+        int right = deepest(root.right);
+        if (result < left + right) {
+            result = left + right;
+        }
+        return Math.max(left, right) + 1;
+    }
+
+    public int diameterOfBinaryTree(TreeNode root) {
+        result = 0;
+        deepest(root);
+        return result;
+    }
 }
